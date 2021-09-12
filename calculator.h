6b7ab2e02e8cfb007aa8642db51752ae6b2bf429
Mateusz Koscielniak <matekk323@gmail.com>
2021-09-12 16:28:31 +0100

    Initial commit, adding gui window setup

---
+++ calculator.h
@@ -0,0 +1,21 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Calculator; }
+QT_END_NAMESPACE
+
+class Calculator : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    Calculator(QWidget *parent = nullptr);
+    ~Calculator();
+
+private:
+    Ui::Calculator *ui;
+};
+#endif // CALCULATOR_H
